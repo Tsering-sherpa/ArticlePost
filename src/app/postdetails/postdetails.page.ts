bf26e324f054090e37624018c314341f56b61122
Tsering Wongdi Sherpa <tseringwongdisherpa@bajratechnologies.com>
2021-10-27 13:12:37 +0545

    FEAT: Added new pages of Home, Add and PostDetails

---
+++ src/app/postdetails/postdetails.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postdetails',
+  templateUrl: './postdetails.page.html',
+  styleUrls: ['./postdetails.page.scss'],
+})
+export class PostdetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
